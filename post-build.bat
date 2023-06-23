0d4f4dca6c6abc0b6fcf92c31e35d0739fb2f769
Lucasion <lucasion@hotmail.com>
2023-06-23 20:05:26 +0200

    Added windows build scripts

---
+++ post-build.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Implement post-build actions here, like calling a deploy script.
+REM All commands must exit with code 0 on success. Anything else is considered failure.
+REM %1 - the path to the build output directory
