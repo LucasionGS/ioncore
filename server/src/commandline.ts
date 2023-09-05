0f32d5ffb1dcb8935eaf56028a9d7d975a41d25d
Lucasion <lucasion@hotmail.com>
2023-09-06 00:24:47 +0200

    Changed to monorepo Altered commandline to allow flags Renamed build.sh to build

--- server/src/commandline.ts
+++ server/src/commandline.ts
@@ -13,20 +13,38 @@ commandline(process.argv.slice(2))
 });
 
 async function commandline(args: string[]) {
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    // Split -XYZ into -X -Y -Z
+    if (arg.startsWith("-") && arg.length > 2) {
+      args.splice(i, 1, ...arg.slice(1).split("").map((arg) => "-" + arg));
+    }
+  }
+  
   if (args.length === 0) {
-    console.log("Usage: yarn \"ic <command>\"");
+    console.log("Usage: yarn ic <command> [options]");
+    console.log("Commands:");
+    console.log("  sync - Sync database");
     return;
   }
   const command = args[0];
   if (command === "sync") {
+    // Alter
+    let alter: boolean | null = args.includes("--alter") || args.includes("-a") || null;
+    alter ??= !args.includes("--no-alter") && null;
+    
+    // Force
+    let force: boolean | null = args.includes("--force") || args.includes("-f") || null;
+    force ??= !args.includes("--no-force") && null;
+    
     const readline = rl.createInterface(process.stdin, process.stdout);
     const question = util.promisify(readline.question).bind(readline) as unknown as (question: string) => Promise<string>;
-    const syncAlter = (await question("Alter database? (y/N): ")).toLowerCase().startsWith("y");
-    const syncForce = (await question("Force database? This will clear the database entirely (y/N): ")).toLowerCase().startsWith("y");
+    alter ??= (await question("Alter database? (y/N): ")).toLowerCase().startsWith("y");
+    force ??= (await question("Force database? This will clear the database entirely (y/N): ")).toLowerCase().startsWith("y");
     // const firstLoginCreateDefaultUser = true;
 
 
-    await sequelize.sync({ alter: syncAlter, force: syncForce });
+    await sequelize.sync({ alter, force });
     console.log("Database synced");
     let userRole = await Role.getRoleByName("user");
     const adminRole = await Role.getRoleByName("admin");
